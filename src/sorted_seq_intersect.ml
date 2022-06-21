0cbc1082c69bc621998a40c1d5a27c02644a5a78
Fermin Reig <fermin.reig@gmail.com>
2022-06-21 22:22:49 +0100

    container type now parameterizable (used to be fixed as array)

--- src/sorted_seq_intersect.ml
+++ src/sorted_seq_intersect.ml
@@ -1,45 +1,63 @@
+(* 
+  Signature for a container type with functions get and length, and an comparsion function for elements of the container
+*)
+module type S = sig
+  type elt
+  type t
+
+  val cmp: elt -> elt -> int
+  val get: t -> int -> elt
+  val length: t -> int
+end
+
 
 (* Intersection by linear search on sorted sequences. Restricted to ranges [lo..hi] and [b_lo..b_hi] *)
-let intersect_merge_range ?(acc=[]) a lo hi b b_lo b_hi = 
+
+let intersect_merge_range (type e) (type t) ?(acc=[]) (module M : S with type t = t and type elt = e) a lo hi b b_lo b_hi =
+  let ( .%{} ) = M.get in
   let rec imerge acc i j = 
     if i>hi || j>b_hi then 
       acc
     else
-      match (compare a.(i) b.(j)) with
+      match (M.cmp a.%{i} b.%{j}) with
       | -1 -> imerge acc (i+1) j
-      |  0 -> imerge (a.(i)::acc) (i+1) (j+1)
+      |  0 -> imerge (a.%{i}::acc) (i+1) (j+1)
       |  _ -> imerge acc i (j+1)
   in
   imerge acc lo b_lo
 
 (* Intersection by linear search on sorted sequences *)
-let intersect_merge a b = intersect_merge_range a 0 (Array.length a - 1)  b 0 (Array.length b - 1)
   
-  
+let intersect_merge (type e) (type t) (module M : S with type t = t and type elt = e) a b = intersect_merge_range (module M) a 0 (M.length a - 1) b 0 (M.length b - 1)
+
+
   
 (* Lookup e in arr by binary search, returning the first index where we'd insert e to preserve the sorted property.
    Returns i = min value in [lo..hi] such that arr[i] >= e 
    If e>arr[i] for every i in [lo..hi], then return hi+1
    Precondition: arr sorted. 
   *)
-let rec lookup e arr lo hi =
-  if lo > hi then
-    lo
-  else 
-    let mid = lo + (hi-lo) / 2 in
-    match compare e arr.(mid) with
-    |  0 -> mid
-    | -1 -> lookup e arr lo (mid-1)
-    |  _ -> lookup e arr (mid+1) hi
 
-
+let lookup (type e) (type t) (module M : S with type t = t and type elt = e) e arr lo hi =
+  let rec look lo hi =
+    if lo > hi then
+      lo
+    else 
+      let mid = lo + (hi-lo) / 2 in
+      match M.cmp e (M.get arr mid) with
+      |  0 -> mid
+      | -1 -> look lo (mid-1)
+      |  _ -> look (mid+1) hi
+  in
+    look lo hi
+  
 
 (* TODO: better name than swap *)
-let[@inline] swap f acc a lo hi b b_lo b_hi =
+let[@inline] swap f a lo hi b b_lo b_hi =
   if hi-lo <= b_hi-b_lo then
-    f acc a lo hi b b_lo b_hi
+    f a lo hi b b_lo b_hi
   else     
-    f acc b b_lo b_hi a lo hi
+    f b b_lo b_hi a lo hi
 
 
 (* Intersection using the Baeza-Yates-Salinger algorithm on sorted sequences
@@ -49,57 +67,60 @@ let[@inline] swap f acc a lo hi b b_lo b_hi =
 
    When we call this function (including in the recursive calls), the first sequence sould be the one of smaller size
 *)
-let rec intersect_bisect_range acc a lo hi b b_lo b_hi =
-  (* the intersection is empty if either array range is empty, or if the values in the arrays don't overlap *)
-  if lo > hi || b_lo > b_hi || a.(lo) > b.(b_hi) || b.(b_lo) > a.(hi) then
-      acc
-  else
-    (* Divide a into two sections, left and right of the median. The two sections are the same size, +/- 1 elem.
-       Divide b into two sections, left and right of the position of a's median in b. B's sections need not be of the same size. *)
-    let mid = lo + (hi-lo) / 2 in
-    let median = a.(mid) in
-    (* possible optimisation: obtain a range in b smaller than lob..hib by bracketing with a.(lo) and a.(hi) in b. But searching has a cost, so we're not guaranteed to reduce the total runtime *)
-    let b_mid = lookup median b b_lo b_hi in
-    (* recurse on the left sections of a and b *)
-    let acc = swap intersect_bisect_range acc a lo (mid-1) b b_lo (b_mid-1) in
-    if b_mid > b_hi then
-      acc
-    else  
+let rec intersect_bisect_range (type e) (type t) a lo hi b b_lo b_hi acc (module M : S with type t = t and type elt = e) =
+  let ( .%{} ) = M.get in
+  let rec isect a lo hi b b_lo b_hi acc = 
+    (* the intersection is empty if either array range is empty, or if the values in the arrays don't overlap *)
+    if lo > hi || b_lo > b_hi || a.%{lo} > b.%{b_hi} || b.%{b_lo} > a.%{hi} then
+        acc
+    else
+      (* Divide a into two sections, left and right of the median. The two sections are the same size, +/- 1 elem.
+        Divide b into two sections, left and right of the position of a's median in b. B's sections need not be of the same size. *)
+      let mid = lo + (hi-lo) / 2 in
+      let median = a.%{mid} in
+      (* possible optimisation: obtain a range in b smaller than lob..hib by bracketing with a.(lo) and a.(hi) in b. But searching has a cost, so we're not guaranteed to reduce the total runtime *)
+      let b_mid = lookup (module M) median b b_lo b_hi in
+      (* recurse on the left sections of a and b *)
+      let acc = swap isect a lo (mid-1) b b_lo (b_mid-1) acc in
       (* recurse on the right sections of a and b *)
-      if median = b.(b_mid) then
-        swap intersect_bisect_range (median::acc) a (mid+1) hi b (b_mid+1) b_hi
+      if M.cmp median b.%{b_mid} = 0 then
+        swap isect a (mid+1) hi b (b_mid+1) b_hi (median::acc)
       else
-        swap intersect_bisect_range acc a (mid+1) hi b b_mid b_hi
+        swap isect a (mid+1) hi b b_mid b_hi acc
+  in
+    isect a lo hi b b_lo b_hi acc
 
 
-let intersect_bisect a b = swap intersect_bisect_range [] a 0 (Array.length a - 1) b 0 (Array.length b - 1)
+let intersect_bisect (type e) (type t) (module M : S with type t = t and type elt = e) a b = swap intersect_bisect_range a 0 (M.length a - 1) b 0 (M.length b - 1) [] (module M)
+
 
 (* Adaptive algorithm: switch to linear merge when m grows wrt to n. The paper suggests once m = 0.031n + 20.696 *)
-let rec intersect_adaptive_range acc a lo hi b b_lo b_hi =
+let rec intersect_adaptive_range (type e) (type t) a lo hi b b_lo b_hi acc (module M : S with type t = t and type elt = e) =
+  let ( .%{} ) = M.get in
+  let rec isect a lo hi b b_lo b_hi acc =
   (* the intersection is empty if either array range is empty, or if the values in the arrays don't overlap *)
-  if lo > hi || b_lo > b_hi || a.(lo) > b.(b_hi) || b.(b_lo) > a.(hi) then
+    if lo > hi || b_lo > b_hi || a.%{lo} > b.%{b_hi} || b.%{b_lo} > a.%{hi} then
       acc
   (* switch to linear merge when m grows wrt n *)
-  else if (hi-lo) * 32 > (b_hi-b_lo) then
-    intersect_merge_range ~acc:acc a lo hi b b_lo b_hi
-  else 
-    (* Divide a into two sections, left and right of the median. The two sections are the same size, +/- 1 elem.
-       Divide b into two sections, left and right of the position of a's median in b. B's sections need not be of the same size. *)
-    let mid = lo + (hi-lo) / 2 in
-    let median = a.(mid) in
-    (* possible optimisation: obtain a range in b smaller than lob..hib by bracketing with a.(lo) and a.(hi) in b. But searching has a cost, so we're not guaranteed to reduce the total runtime *)
-    let b_mid = lookup median b b_lo b_hi in
-    (* recurse on the left sections of a and b *)
-    let acc = swap intersect_adaptive_range acc a lo (mid-1) b b_lo (b_mid-1) in
-    if b_mid > b_hi then
-      acc
-    else  
+    else if (hi-lo) * 32 > (b_hi-b_lo) then
+      intersect_merge_range  ~acc:acc (module M) a lo hi b b_lo b_hi 
+    else 
+      (* Divide a into two sections, left and right of the median. The two sections are the same size, +/- 1 elem.
+        Divide b into two sections, left and right of the position of a's median in b. B's sections need not be of the same size. *)
+      let mid = lo + (hi-lo) / 2 in
+      let median = a.%{mid} in
+      (* possible optimisation: obtain a range in b smaller than lob..hib by bracketing with a.(lo) and a.(hi) in b. But searching has a cost, so we're not guaranteed to reduce the total runtime *)
+      let b_mid = lookup (module M) median b b_lo b_hi in
+      (* recurse on the left sections of a and b *)
+      let acc = swap isect a lo (mid-1) b b_lo (b_mid-1) acc in
       (* recurse on the right sections of a and b *)
-      if median = b.(b_mid) then
-        swap intersect_adaptive_range (median::acc) a (mid+1) hi b (b_mid+1) b_hi
+      if M.cmp median b.%{b_mid} = 0 then
+        swap isect a (mid+1) hi b (b_mid+1) b_hi (median::acc) 
       else
-        swap intersect_adaptive_range acc a (mid+1) hi b b_mid b_hi
+        swap isect a (mid+1) hi b b_mid b_hi acc
+  in
+    isect a lo hi b b_lo b_hi acc
 
 
-let intersect_bisect_adaptive a b = swap intersect_adaptive_range [] a 0 (Array.length a - 1) b 0 (Array.length b - 1)
+let intersect_bisect_adaptive (type e) (type t) (module M : S with type t = t and type elt = e) a b = swap intersect_adaptive_range a 0 (M.length a - 1) b 0 (M.length b - 1) [] (module M) 
 
--- src/sorted_seq_intersect.mli
+++ src/sorted_seq_intersect.mli
@@ -1,33 +1,47 @@
 (** 
+  Signature for a container type with functions get and length, and an comparsion function for elements in the container
+*)
+module type S = sig
+  type elt
+  type t
+
+  val cmp: elt -> elt -> int
+  val get: t -> int -> elt
+  val length: t -> int
+end
+
+
+(** 
   Intersection of sorted sequences, using a linear merge algorithm, with complexity O(m+n).
 *)
-val intersect_merge: 'a array -> 'a array -> 'a list
+val intersect_merge: (module S with type elt = 'a and type t = 't) -> 't -> 't -> 'a list
 
 (**
   Like [intersect_merge], but restricted to ranges a[lo..hi], b[b_lo..b_hi]
 *)
-val intersect_merge_range: ?acc:'a list -> 'a array -> int -> int -> 'a array -> int -> int -> 'a list
+val intersect_merge_range: ?acc:'a list -> (module S with type elt = 'a and type t = 't) ->
+  't -> int -> int -> 't -> int -> int -> 'a list
 
 (** 
   Intersection of sorted sequences, using the Baeza-Yates-Salinger algorithm, with complexity O(m log(n/m)).
   If m is o(n/log n), this algorithm is better than linear merging, which is O(m+n)
   If m << n, it's better to do m binary searches in b
 *)
-val intersect_bisect: 'a array -> 'a array -> 'a list
+val intersect_bisect: (module S with type elt = 'a and type t = 't) -> 't -> 't -> 'a list
 
 (**
   Like [intersect_bisect_merge], but restricted to ranges a[lo..hi], b[b_lo..b_hi]
 *)
-val intersect_bisect_range: 'a list -> 'a array -> int -> int -> 'a array -> int -> int -> 'a list
+val intersect_bisect_range: 't -> int -> int -> 't -> int -> int -> 'a list -> (module S with type elt = 'a and type t = 't) -> 'a list
 
 (** 
   Intersection of sorted sequences, using the adaptive Baeza-Yates-Salinger algorithm, which switches to a merge algorithm when n/m < 32.
   If m is o(n/log n), this algorithm is better than linear merging, which is O(m+n)
   If m << n, it's better to do m binary searches in b
 *)
-val intersect_bisect_adaptive: 'a array -> 'a array -> 'a list
+val intersect_bisect_adaptive: (module S with type elt = 'a and type t = 't) -> 't -> 't -> 'a list
 
 (**
   Like [intersect_bisect_adaptive], but restricted to ranges a[lo..hi], b[b_lo..b_hi]
 *)
-val intersect_adaptive_range: 'a list -> 'a array -> int -> int -> 'a array -> int -> int -> 'a list
+val intersect_adaptive_range: 't -> int -> int -> 't -> int -> int -> 'a list -> (module S with type elt = 'a and type t = 't) -> 'a list
